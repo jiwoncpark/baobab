78bd5a9e0e8a5ed7ab07ba3c6df3286a3f838af7
jiwoncpark <jiwon.christine.park@gmail.com>
2019-09-06 15:23:01 -0700

    Switch to latest theme, take 2

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,9 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+import sphinx_rtd_theme
+
+extensions = ['sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,7 +51,7 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'latest'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
