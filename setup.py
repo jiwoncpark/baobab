ee7ca114123d27b19411e40ddc6a6303af8301f2
jiwoncpark <jiwon.christine.park@gmail.com>
2019-11-08 15:13:42 -0800

    Remove manual install_requires

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 from setuptools import setup, find_packages
-#print(find_packages())
-required_packages = []
-with open('requirements.txt') as f:
-    required_packages = f.read().splitlines()
 #print(reqs)
 
 setup(
@@ -16,9 +12,9 @@ setup(
       long_description=open("README.rst").read(),
       long_description_content_type='text/markdown',
       url='https://github.com/jiwoncpark/baobab',
-      install_requires=required_packages,
-      dependency_links=[
-      'http://github.com/sibirrer/fastell4py/tarball/master#egg=fastell4py',],
+      #install_requires=required_packages,
+      #dependency_links=[
+      #'http://github.com/sibirrer/fastell4py/tarball/master#egg=fastell4py',],
       include_package_data=True,
       entry_points={
       'console_scripts': ['generate=baobab.generate:main', 'to_hdf5=baobab.to_hdf5:main'],
