f2617548f0d1e15cd47b50d3368bf7b39050a26e
jiwoncpark <jiwon.christine.park@gmail.com>
2019-12-10 01:41:14 -0800

    Use dictionary indexing, not class attribute indexing in case argument is python's native dict

--- baobab/sim_utils/psf_utils.py
+++ baobab/sim_utils/psf_utils.py
@@ -21,18 +21,18 @@ def get_PSF_models(psf_config, pixel_scale):
         
     """ 
     psf_models = []
-    if psf_config.type == 'PIXEL':
-        if psf_config.which_psf_maps is None:
+    if psf_config['type'] == 'PIXEL':
+        if psf_config['which_psf_maps'] is None:
             # Instantiate PSF with all available PSF maps
             #FIXME: equate psf_id with psf_i since seed number is meaningless
             psf_id_list = [101, 102, 103, 104, 105, 107, 108, 109, 110, 111, 113, 114, 115, 116, 117, 118]
         else:
-            psf_id_list = [psf_config.which_psf_maps]
+            psf_id_list = [psf_config['which_psf_maps']]
 
         for psf_i, psf_id in enumerate(psf_id_list):
             psf_path = resource_filename('baobab.in_data', 'psf_maps/psf_{:d}.fits'.format(psf_id))
             psf_map = pyfits.getdata(psf_path)
-            kernel_cut = kernel_util.cut_psf(psf_map, psf_config.kernel_size)
+            kernel_cut = kernel_util.cut_psf(psf_map, psf_config['kernel_size'])
             kwargs_psf = {'psf_type': 'PIXEL', 'pixel_size': pixel_scale, 'kernel_point_source': kernel_cut}
             psf_models.append(PSF(**kwargs_psf))
     else:
