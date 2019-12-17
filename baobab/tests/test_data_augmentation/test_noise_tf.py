aada4b6d857c0896156e6e24c62b5a02f99f38c6
jiwoncpark <jiwon.christine.park@gmail.com>
2019-12-16 17:40:51 -0800

    Force CPU usage

--- baobab/tests/test_data_augmentation/test_noise_tf.py
+++ baobab/tests/test_data_augmentation/test_noise_tf.py
@@ -68,6 +68,7 @@ class TestNoiseTF(unittest.TestCase):
         """Test whether tf.data.Dataset can be instantiated from tf.data.TFRecordDataset with the data augmentation (noise addition) mapping
 
         """
+        os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
         tf_record_path = os.path.abspath('test_ADU')
         batch_size = 2
         n_epochs = 3
